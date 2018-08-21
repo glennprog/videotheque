4c611fdc127d0f4a8f7f045c06306f9af32813e6
Glenn <glenn.milingui@gmail.com>
2018-08-21 14:09:56 +0200

    maj master

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,7 +15,8 @@ class DefaultController extends AbstractController
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            //'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => 'path disabled...'
         ]);
     }
 }
